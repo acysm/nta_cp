77726833773150eb39574315600ba03d7cf5e041
Dremlyuga Nikita <koimizuni@gmail.com>
2024-12-17 01:22:01 +0200

    add time limit function

--- BruteForceDLP.py
+++ BruteForceDLP.py
@@ -1,12 +1,21 @@
-def discrete_log(a, b, p):
+import time
+
+def discrete_log(a, b, p, time_limit=300):
+
+    start_time = time.time()
+
     for x in range(p - 1):
         if pow(a, x, p) == b:
+            elapsed_time = time.time() - start_time
+            print(f"Час виконання: {elapsed_time:.2f} секунд")
             return x
-    return None
+        if time.time() - start_time > time_limit:
+            print("Час вичерпано")
+            return None
 
-a = 3  # генератор
-b = 6  # елемент
-p = 7  # модуль
+a = 342 # генератор
+b = 4297301 # елемент
+p = 5786947 # модуль
 
 log_result = discrete_log(a, b, p)
 
